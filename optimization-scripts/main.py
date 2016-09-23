627a008775598f70764cd51fc4e56a4dca86e2fc
Ryan <rlober@gmail.com>
2016-09-23 17:22:04 +0200

    Add client process

--- optimization-scripts/main.py
+++ optimization-scripts/main.py
@@ -2,39 +2,57 @@ import subprocess
 import time
 import shlex
 
-# Gazebo world file
-pathToIcubGazeboWorlds = "/home/ryan/Code/iCub_Software/icub-gazebo/world"
-icubWorldPath = pathToIcubGazeboWorlds + "/icub.world"
+rootPath = "/home/ryan"
 
-# Task set path
-allTaskSets = "/home/ryan/Code/bayesian-task-optimization/reaching-task-sets/icubGazeboSim"
-taskSetPath = allTaskSets + "/TaskOptimizationTaskSet.xml"
+def executeWaypoints(pathToRightHandWptFile, pathToComWptFile):
 
+    # Gazebo world file
+    pathToIcubGazeboWorlds = rootPath + "/icub-gazebo/world"
+    icubWorldPath = pathToIcubGazeboWorlds + "/icub.world"
 
-print('Starting script...')
-print('-- Launching yarpserver')
-yarp = subprocess.Popen(["yarpserver"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-time.sleep(1)
-
-print('-- Launching gzserver with icub.world @', icubWorldPath)
-gazebo = subprocess.Popen(["gzserver", icubWorldPath], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-time.sleep(4)
+    # Task set path
+    allTaskSets = rootPath + "/bayesian-task-optimization/reaching-task-sets/icubGazeboSim"
+    taskSetPath = allTaskSets + "/TaskOptimizationTaskSet.xml"
 
 
-args1 = "ocra-icub-server --floatingBase --controllerType HOCRA --solver QPOASES --taskSet " + taskSetPath + " --absolutePath"
-args = shlex.split(args1)
-print('-- Launching ocra-icub-server with args: ', args)
-controller = subprocess.Popen(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    print('Starting script...')
+    print('-- Launching yarpserver')
+    yarp = subprocess.Popen(["yarpserver"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    time.sleep(1)
+
+    print('-- Launching gzserver with icub.world @', icubWorldPath)
+    gazebo = subprocess.Popen(["gzserver", icubWorldPath], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    time.sleep(4)
 
 
-time.sleep(10)
+    args1 = "ocra-icub-server --floatingBase --controllerType HOCRA --solver QPOASES --taskSet " + taskSetPath + " --absolutePath"
+    args = shlex.split(args1)
+    print('-- Launching ocra-icub-server with args: ', args)
+    controller = subprocess.Popen(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+    time.sleep(5)
+
+    args1 = "reach-client --rightHandWptFile "+pathToRightHandWptFile+" --comWptFile "+pathToComWptFile
+    args = shlex.split(args1)
+    print('-- Launching reach-client with args: ', args)
+    client = subprocess.Popen(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+    client.wait()
 
 
-print('-- Terminating controller')
-controller.terminate()
-controller.wait()
-print('-- Terminating gzserver')
-gazebo.terminate()
-gazebo.wait()
-print('-- Terminating yarpserver')
-yarp.terminate()
+    print('-- Terminating controller')
+    controller.terminate()
+    controller.wait()
+    print('-- Terminating gzserver')
+    gazebo.terminate()
+    gazebo.wait()
+    print('-- Terminating yarpserver')
+    yarp.terminate()
+
+
+
+
+pathToRightHandWptFile = "/home/ryan/bayesian-task-optimization/rightHandWaypoints.txt"
+pathToComWptFile = "/home/ryan/bayesian-task-optimization/comWaypoints.txt"
+
+executeWaypoints(pathToRightHandWptFile, pathToComWptFile)
