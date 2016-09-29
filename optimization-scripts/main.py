37fe293af03d1c2e4eb941d1e20012a2461202e3
Ryan <rlober@gmail.com>
2016-09-29 14:57:09 +0200

    use optim module in main

--- optimization-scripts/main.py
+++ optimization-scripts/main.py
@@ -1,35 +1,15 @@
 import numpy as np
-from files import *
-from simulate import *
+from optim import *
 import os
 
 home_path = os.path.expanduser("~")
-
-rightHandStartingWaypoints = np.array([[0.24, -0.27, 0.64],[0.30, -0.10, 0.54],[0.36,  0.00, 0.44]])
-comStartingWaypoints = np.array([[0.024, -0.060, 0.500],[0.025, -0.061, 0.501],[0.026, -0.062, 0.502]])
-
-
-
 root_path = home_path + "/Optimization_Tests/"
-trial_dir_name = "Test_" + getDateAndTimeString()
-trial_dir_path = root_path + trial_dir_name + "/"
-os.makedirs(trial_dir_path)
 
+right_hand_starting_waypoints = np.array([[0.24, -0.27, 0.64],[0.30, -0.10, 0.54],[0.36,  0.00, 0.44]])
+com_starting_waypoints = np.array([[0.024, -0.060, 0.500],[0.025, -0.061, 0.501],[0.026, -0.062, 0.502]])
 
-optimization_iteration = 0
-iteration_dir_name = "Iteration_" + str(optimization_iteration).zfill(3)
-iteration_dir_path = trial_dir_path + iteration_dir_name + "/"
-os.makedirs(iteration_dir_path)
+robo_task = ReachingWithBalance(root_path, right_hand_starting_waypoints, com_starting_waypoints)
+solver = initializeRoboSolver(robo_task)
 
-pathToRightHandWptFile = iteration_dir_path + "/rightHandWaypoints.txt"
-pathToComWptFile = iteration_dir_path + "/comWaypoints.txt"
-
-np.savetxt(pathToRightHandWptFile, rightHandStartingWaypoints)
-np.savetxt(pathToComWptFile, comStartingWaypoints)
-
-executeWaypoints(pathToRightHandWptFile, pathToComWptFile, iteration_dir_path)
-
-taskData = getDataFromFiles(iteration_dir_path)
-
-for t in taskData:
-    t.printData()
+max_iter = 2
+solver.run(max_iter, robo_task.X_init, robo_task.Y_init)
