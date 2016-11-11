2e6441374b873606dd20b5ca67de8d942d5fa2c8
Ryan Lober <rlober@gmail.com>
2016-11-11 12:20:37 +0100

    adjust starting wpts to 1 wpt per task

--- optimization-scripts/main.py
+++ optimization-scripts/main.py
@@ -5,14 +5,14 @@ import os
 home_path = os.path.expanduser("~")
 root_path = home_path + "/Optimization_Tests/"
 
-right_hand_starting_waypoints = np.array([[0.24, -0.27, 0.64],[0.30, -0.10, 0.54],[0.36,  0.00, 0.44]])
-com_starting_waypoints = np.array([[0.024, -0.060, 0.500],[0.025, -0.061, 0.501],[0.026, -0.062, 0.502]])
+right_hand_starting_waypoints = np.array([[0.36, -0.23, 0.5]])
+com_starting_waypoints = np.array([[0.015, -0.11, 0.51]])
 
 robo_task = ReachingWithBalance(root_path, right_hand_starting_waypoints, com_starting_waypoints, True)
 
 
-#
-# # st = "RoBO"
-# # st = "BayesOpt"
+
+st = "RoBO"
+# st = "BayesOpt"
 # st = "cmaes"
-# runOptimization(robo_task, max_iter=30, solver_type=st, cost_saturation=1000.0)
+runOptimization(robo_task, max_iter=10, solver_type=st, cost_saturation=1000.0)
