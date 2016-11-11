db52e0209eea66382938edfac0c6a89c6fa6a8f8
Ryan Lober <rlober@gmail.com>
2016-11-11 12:31:22 +0100

    play from regular wpts if no optimal wpts found

--- optimization-scripts/replay.py
+++ optimization-scripts/replay.py
@@ -15,5 +15,11 @@ if file_path != "":
     iteration_directory = os.path.split(file_path)[0]
     print("Replaying from: ", iteration_directory)
     pathToRightHandWptFile = iteration_directory + "/rightHandWaypoints_optimal.txt"
+    if not os.path.exists(pathToRightHandWptFile):
+        pathToRightHandWptFile = iteration_directory + "/rightHandWaypoints.txt"
+
     pathToComWptFile = iteration_directory + "/comWaypoints_optimal.txt"
+    if not os.path.exists(pathToComWptFile):
+        pathToComWptFile = iteration_directory + "/comWaypoints.txt"
+
     simulate(pathToRightHandWptFile, pathToComWptFile, savePath=None, verbose=True, visual=True, askUserForReplay=True, goToHome=True)
