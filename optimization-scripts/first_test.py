0246533c8b9ab0dfd2c19eb8268ac38867d9150a
Ryan <rlober@gmail.com>
2016-11-23 15:26:59 +0100

    Minor changes

--- optimization-scripts/first_test.py
+++ optimization-scripts/first_test.py
@@ -12,11 +12,11 @@ com_starting_waypoints = np.array([[0.015, -0.11, 0.51]])
 
 first_test = OneComWaypointStaticTest(root_path, right_hand_starting_waypoints, com_starting_waypoints)
 
-# solver_parameters = {'max_iter':30, 'par':0.01, 'kernel':'RBF', 'maximizer':'CMAES'}
-# solver = RoboSolver(first_test, solver_parameters)
+solver_parameters = {'max_iter':30, 'par':0.01, 'kernel':'RBF', 'maximizer':'CMAES'}
+solver = RoboSolver(first_test, solver_parameters)
 
-solver_parameters = {'max_iter':50, 'initial_sigma':0.5}
-solver = CmaSolver(first_test, solver_parameters)
+# solver_parameters = {'max_iter':30, 'initial_sigma':0.5}
+# solver = CmaSolver(first_test, solver_parameters)
 
 solver.optimize()
 
