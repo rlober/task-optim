2a2188e6fac4c9f44b605c50d57ce0fdf3c0d2f2
Ryan Lober <rlober@gmail.com>
2016-11-23 19:42:34 +0100

    Change acquisition fn

--- optimization-scripts/first_test.py
+++ optimization-scripts/first_test.py
@@ -5,14 +5,14 @@ from task_optim.solvers.robo_solver import RoboSolver
 from task_optim.solvers.cma_solver import CmaSolver
 
 home_path = os.path.expanduser("~")
-root_path = home_path + "/Optimization_Tests/OneComWaypointStaticTest/"
+root_path = home_path + "/Optimization_Tests/Blah/"
 
 right_hand_starting_waypoints = np.array([[0.36, -0.23, 0.5]])
 com_starting_waypoints = np.array([[0.015, -0.11, 0.51]])
 
 first_test = OneComWaypointStaticTest(root_path, right_hand_starting_waypoints, com_starting_waypoints)
 
-solver_parameters = {'max_iter':30, 'par':0.01, 'kernel':'RBF', 'maximizer':'CMAES'}
+solver_parameters = {'max_iter':30, 'par':0.01, 'kernel':'RBF', 'acquisition':'EI', 'maximizer':'CMAES'}
 solver = RoboSolver(first_test, solver_parameters)
 
 # solver_parameters = {'max_iter':30, 'initial_sigma':0.5}
