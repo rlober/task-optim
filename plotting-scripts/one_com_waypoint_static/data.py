1cc2384a2155fe27df4038fabb6654a0b28eb541
Ryan <rlober@gmail.com>
2016-12-05 10:16:20 +0100

    Factorize palettes and plotting

--- plotting-scripts/one_com_waypoint_static/data.py
+++ plotting-scripts/one_com_waypoint_static/data.py
@@ -30,17 +30,21 @@ class TestData():
     def extractDataFromTest(self):
         self.iteration_data = []
         # rh = right hand
-        for d in self.iter_dirs:
-            com_task_data, rh_task_data = getDataFromFiles(d)
-            self.iteration_data.append([com_task_data, rh_task_data])
+        # for d in self.iter_dirs:
+        #     com_task_data, rh_task_data = getDataFromFiles(d)
+        #     self.iteration_data.append([com_task_data, rh_task_data])
+        #
+        # com_task_data, rh_task_data = getDataFromFiles(self.iter_dirs[0])
+        # self.original_data = [com_task_data, rh_task_data]
 
-        com_task_data, rh_task_data = getDataFromFiles(self.iter_dirs[0])
-        self.original_data = [com_task_data, rh_task_data]
 
-        com_task_data, rh_task_data = getDataFromFiles(self.opt_dir)
+        com_task_data, rh_task_data = getDataFromFiles(self.opt_dir[0])
         self.optimal_data = [com_task_data, rh_task_data]
 
 
     def printInfoAboutTest(self):
 
         print("I found "+str(self.n_iterations)+" iteration directories in the test directory: "+self.test_dir)
+
+    def generatePlots(self):
+        pass
