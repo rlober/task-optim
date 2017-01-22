f394964a1034c96b0df5bffff5251a2c1db80509
Ryan <rlober@gmail.com>
2017-01-22 17:49:13 +0100

    Check if orig and opt exist first

--- video-scripts/combine_reaching_videos.py
+++ video-scripts/combine_reaching_videos.py
@@ -5,62 +5,65 @@ import time
 import shlex
 
 def combineVideos(test_dir):
-    ffmpeg_args = """ffmpeg -y -i """ + test_dir + """/original.mp4 -i """ + test_dir + """/optimal.mp4 -filter_complex "[0:v][1:v]hstack=inputs=2[v]" -map "[v]" -ac 2 """+test_dir+"""/temp_0.mp4"""
+    if (os.path.exists(os.path.join(test_dir, "original.mp4"))) and (os.path.exists(os.path.join(test_dir, "optimal.mp4"))):
+        ffmpeg_args = """ffmpeg -y -i """ + test_dir + """/original.mp4 -i """ + test_dir + """/optimal.mp4 -filter_complex "[0:v][1:v]hstack=inputs=2[v]" -map "[v]" -ac 2 """+test_dir+"""/temp_0.mp4"""
 
-    ffmpeg_args = shlex.split(ffmpeg_args)
-    proc_1 = subprocess.Popen(ffmpeg_args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    proc_1.wait()
+        ffmpeg_args = shlex.split(ffmpeg_args)
+        proc_1 = subprocess.Popen(ffmpeg_args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        proc_1.wait()
 
-    ffmpeg_args = """ffmpeg -i """ + test_dir + """/temp_0.mp4 -y -vf drawtext="text='ORIGINAL':\
-    fontcolor=white:\
-    fontsize=48:\
-    box=1:\
-    boxcolor=black@0.8:\
-    boxborderw=10:\
-    x=(main_w)/4:\
-    y=main_h-(text_h*2)" -codec:a copy """ + test_dir + """/temp_1.mp4"""
+        ffmpeg_args = """ffmpeg -i """ + test_dir + """/temp_0.mp4 -y -vf drawtext="text='ORIGINAL':\
+        fontcolor=white:\
+        fontsize=48:\
+        box=1:\
+        boxcolor=black@0.8:\
+        boxborderw=10:\
+        x=(main_w)/4:\
+        y=main_h-(text_h*2)" -codec:a copy """ + test_dir + """/temp_1.mp4"""
 
-    ffmpeg_args = shlex.split(ffmpeg_args)
-    proc_1 = subprocess.Popen(ffmpeg_args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    proc_1.wait()
+        ffmpeg_args = shlex.split(ffmpeg_args)
+        proc_1 = subprocess.Popen(ffmpeg_args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        proc_1.wait()
 
-    ffmpeg_args = """ffmpeg -i """ + test_dir + """/temp_1.mp4 -y -vf drawtext="text='OPTIMAL':\
-    fontcolor=white:\
-    fontsize=48:\
-    box=1:\
-    boxcolor=black@0.8:\
-    boxborderw=10:\
-    x=(main_w)*3/4:\
-    y=main_h-(text_h*2)" -codec:a copy """ + test_dir + """/temp_2.mp4"""
+        ffmpeg_args = """ffmpeg -i """ + test_dir + """/temp_1.mp4 -y -vf drawtext="text='OPTIMAL':\
+        fontcolor=white:\
+        fontsize=48:\
+        box=1:\
+        boxcolor=black@0.8:\
+        boxborderw=10:\
+        x=(main_w)*3/4:\
+        y=main_h-(text_h*2)" -codec:a copy """ + test_dir + """/temp_2.mp4"""
 
-    ffmpeg_args = shlex.split(ffmpeg_args)
-    proc_1 = subprocess.Popen(ffmpeg_args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    proc_1.wait()
+        ffmpeg_args = shlex.split(ffmpeg_args)
+        proc_1 = subprocess.Popen(ffmpeg_args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        proc_1.wait()
 
-    video_path = test_dir + "/optimal_and_original.mp4"
+        video_path = test_dir + "/optimal_and_original.mp4"
 
 
-    ffmpeg_args = """ffmpeg -i """ + test_dir + """/temp_2.mp4 -y -vf drawtext="text='"""+video_path+"""':\
-    fontcolor=white:\
-    fontsize=24:\
-    box=1:\
-    boxcolor=black@0.8:\
-    boxborderw=10:\
-    x=(main_w)/2:\
-    y=(text_h*2)" -codec:a copy """ + test_dir + """/optimal_and_original.mp4"""
+        ffmpeg_args = """ffmpeg -i """ + test_dir + """/temp_2.mp4 -y -vf drawtext="text='"""+video_path+"""':\
+        fontcolor=white:\
+        fontsize=24:\
+        box=1:\
+        boxcolor=black@0.8:\
+        boxborderw=10:\
+        x=(main_w)/2:\
+        y=(text_h*2)" -codec:a copy """ + test_dir + """/optimal_and_original.mp4"""
 
-    ffmpeg_args = shlex.split(ffmpeg_args)
-    proc_1 = subprocess.Popen(ffmpeg_args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    proc_1.wait()
+        ffmpeg_args = shlex.split(ffmpeg_args)
+        proc_1 = subprocess.Popen(ffmpeg_args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        proc_1.wait()
 
-    ffmpeg_args = """rm """+test_dir+"""/temp_0.mp4 """+test_dir+"""/temp_1.mp4 """+test_dir+"""/temp_2.mp4"""
+        ffmpeg_args = """rm """+test_dir+"""/temp_0.mp4 """+test_dir+"""/temp_1.mp4 """+test_dir+"""/temp_2.mp4"""
 
-    ffmpeg_args = shlex.split(ffmpeg_args)
-    proc_1 = subprocess.Popen(ffmpeg_args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    proc_1.wait()
+        ffmpeg_args = shlex.split(ffmpeg_args)
+        proc_1 = subprocess.Popen(ffmpeg_args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        proc_1.wait()
 
-    print("View video at:\n", video_path)
-    return video_path
+        print("View video at:\n", video_path)
+        return video_path
+    else:
+        print("ERROR - Test at:", test_dir, "is missing videos.")
 
 
 def concatenateTestVideos(root_tests_dir, destination_dir, video_name):
