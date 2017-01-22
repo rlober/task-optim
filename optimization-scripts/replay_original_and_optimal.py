a2d1c72411f9fceb79620f04422a9902419e154f
Ryan <rlober@gmail.com>
2017-01-22 10:13:14 +0100

    More video functions

--- optimization-scripts/replay_original_and_optimal.py
+++ optimization-scripts/replay_original_and_optimal.py
@@ -6,46 +6,6 @@ import subprocess
 import time
 import shlex
 
-def combineVideos(test_dir):
-    ffmpeg_args = """ffmpeg -y -i """ + test_dir + """/original.mp4 -i """ + test_dir + """/optimal.mp4 -filter_complex "[0:v][1:v]hstack=inputs=2[v]" -map "[v]" -ac 2 """+test_dir+"""/temp_0.mp4"""
-
-    ffmpeg_args = shlex.split(ffmpeg_args)
-    proc_1 = subprocess.Popen(ffmpeg_args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    proc_1.wait()
-
-    ffmpeg_args = """ffmpeg -i """ + test_dir + """/temp_0.mp4 -y -vf drawtext="text='ORIGINAL':\
-    fontcolor=white:\
-    fontsize=48:\
-    box=1:\
-    boxcolor=black@0.8:\
-    boxborderw=10:\
-    x=(main_w)/4:\
-    y=main_h-(text_h*2)" -codec:a copy """ + test_dir + """/temp_1.mp4"""
-
-    ffmpeg_args = shlex.split(ffmpeg_args)
-    proc_1 = subprocess.Popen(ffmpeg_args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    proc_1.wait()
-
-    ffmpeg_args = """ffmpeg -i """ + test_dir + """/temp_1.mp4 -y -vf drawtext="text='OPTIMAL':\
-    fontcolor=white:\
-    fontsize=48:\
-    box=1:\
-    boxcolor=black@0.8:\
-    boxborderw=10:\
-    x=(main_w)*3/4:\
-    y=main_h-(text_h*2)" -codec:a copy """ + test_dir + """/optimal_and_original.mp4"""
-
-    ffmpeg_args = shlex.split(ffmpeg_args)
-    proc_1 = subprocess.Popen(ffmpeg_args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    proc_1.wait()
-
-    ffmpeg_args = """rm """+test_dir+"""/temp_0.mp4 """+test_dir+"""/temp_1.mp4"""
-
-    ffmpeg_args = shlex.split(ffmpeg_args)
-    proc_1 = subprocess.Popen(ffmpeg_args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    proc_1.wait()
-
-    print("View video at:\n", test_dir + "/optimal_and_original.mp4")
 
 def replayOriginalAndOptimalReachingSimulation(test_dir):
 
@@ -63,39 +23,50 @@ def replayOriginalAndOptimalReachingSimulation(test_dir):
 
     client_args_optimal = "reach-client --rightHandWptFile "+ pathToRightHandWptFile +" --comWptFile " + pathToComWptFileOptimal + " --home --record --recordDir " + test_dir + "/ --recordName optimal"
 
-    max_trials = 20
-
-    number_of_trials = 0
-    while number_of_trials <= max_trials:
-        try:
+    original_failed = []
+    optimal_failed = []
+    max_trials = 5
+    if not os.path.exists( os.path.join(test_dir, "original.mp4") ):
+        number_of_trials = 0
+        while number_of_trials <= max_trials:
             simulate(controller_args, client_args_original, gazebo_args, verbose=False, visual=False, askUserForReplay=False, runningRemotely=True)
-            break
-        except:
-            print("Simulation failed. Rerunning. Current number of trials rerun: "+str(number_of_trials))
-            killProcesses()
-            time.sleep(2.0)
-            killProcesses()
-            number_of_trials += 1
 
-    if number_of_trials > max_trials:
-        print("Couldn't get the original simulation to work after "+str(max_trials)+". I am giving up.")
+            if os.path.exists( os.path.join(test_dir, "original.mp4") ):
+                break
+            else:
+                print("Simulation failed. Rerunning. Current number of trials rerun: "+str(number_of_trials))
+                killProcesses()
+                time.sleep(2.0)
+                killProcesses()
+                number_of_trials += 1
 
-    number_of_trials = 0
-    while number_of_trials <= max_trials:
-        try:
+        if number_of_trials > max_trials:
+            print("Couldn't get the original simulation to work after "+str(max_trials)+". I am giving up.")
+            original_failed.append(test_dir)
+    else:
+        print("Video already exists:", os.path.join(test_dir, "original.mp4"))
+
+    if not os.path.exists( os.path.join(test_dir, "optimal.mp4") ):
+        number_of_trials = 0
+        while number_of_trials <= max_trials:
             simulate(controller_args, client_args_optimal, gazebo_args, verbose=False, visual=False, askUserForReplay=False, runningRemotely=True)
-            break
-        except:
-            print("Simulation failed. Rerunning. Current number of trials rerun: "+str(number_of_trials))
-            killProcesses()
-            time.sleep(2.0)
-            killProcesses()
-            number_of_trials += 1
 
-    if number_of_trials > max_trials:
-        print("Couldn't get the optimal simulation to work after "+str(max_trials)+". I am giving up.")
+            if os.path.exists( os.path.join(test_dir, "optimal.mp4") ):
+                break
+            else:
+                print("Simulation failed. Rerunning. Current number of trials rerun: "+str(number_of_trials))
+                killProcesses()
+                time.sleep(2.0)
+                killProcesses()
+                number_of_trials += 1
 
+        if number_of_trials > max_trials:
+            print("Couldn't get the optimal simulation to work after "+str(max_trials)+". I am giving up.")
+            optimal_failed.append(test_dir)
+    else:
+        print("Video already exists:", os.path.join(test_dir, "optimal.mp4"))
 
+    return original_failed, optimal_failed
 
 root_tests_dir = os.path.expanduser("~") + "/Optimization_Tests/rand_right_hand_target_tests/bo/"
 test_name = 'OneComWaypointStaticTest'
@@ -104,8 +75,7 @@ test_dirs = sorted([os.path.join(root_tests_dir, d) for d in dirs if re.match(te
 
 for i, t in enumerate(test_dirs):
     print('Recording simulation for test', i+1, 'of', len(test_dirs), '.', (i+1)/len(test_dirs)*100, "% complete.")
-    try:
-        replayOriginalAndOptimalReachingSimulation(t)
-        combineVideos(t)
-    except:
-        print("Couldn't record simulation from:\n", t)
+    original_failed, optimal_failed = replayOriginalAndOptimalReachingSimulation(t)
+
+print("\n\noriginal_failed\n", original_failed)
+print("\n\noptimal_failed\n", optimal_failed)
--- video-scripts/combine_reaching_videos.py
+++ video-scripts/combine_reaching_videos.py
@@ -31,36 +31,107 @@ def combineVideos(test_dir):
     boxcolor=black@0.8:\
     boxborderw=10:\
     x=(main_w)*3/4:\
-    y=main_h-(text_h*2)" -codec:a copy """ + test_dir + """/optimal_and_original.mp4"""
+    y=main_h-(text_h*2)" -codec:a copy """ + test_dir + """/temp_2.mp4"""
 
     ffmpeg_args = shlex.split(ffmpeg_args)
     proc_1 = subprocess.Popen(ffmpeg_args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     proc_1.wait()
 
-    ffmpeg_args = """rm """+test_dir+"""/temp_0.mp4 """+test_dir+"""/temp_1.mp4"""
+    video_path = test_dir + "/optimal_and_original.mp4"
+
+
+    ffmpeg_args = """ffmpeg -i """ + test_dir + """/temp_2.mp4 -y -vf drawtext="text='"""+video_path+"""':\
+    fontcolor=white:\
+    fontsize=24:\
+    box=1:\
+    boxcolor=black@0.8:\
+    boxborderw=10:\
+    x=(main_w)/2:\
+    y=(text_h*2)" -codec:a copy """ + test_dir + """/optimal_and_original.mp4"""
 
     ffmpeg_args = shlex.split(ffmpeg_args)
     proc_1 = subprocess.Popen(ffmpeg_args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     proc_1.wait()
 
-    print("View video at:\n", test_dir + "/optimal_and_original.mp4")
+    ffmpeg_args = """rm """+test_dir+"""/temp_0.mp4 """+test_dir+"""/temp_1.mp4 """+test_dir+"""/temp_2.mp4"""
 
+    ffmpeg_args = shlex.split(ffmpeg_args)
+    proc_1 = subprocess.Popen(ffmpeg_args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    proc_1.wait()
+
+    print("View video at:\n", video_path)
+    return video_path
+
+
+def concatenateTestVideos(root_tests_dir, destination_dir, video_name):
+    test_name = 'OneComWaypointStaticTest'
+    dirs = [d for d in os.listdir(root_tests_dir) if os.path.isdir(os.path.join(root_tests_dir, d))]
+    test_dirs = sorted([os.path.join(root_tests_dir, d) for d in dirs if re.match(test_name+'.*', d)])
+
+    errored_dirs = []
+
+    cat_file_path = os.path.join(root_tests_dir,"ffmpeg_cat_list.txt")
+    cat_file = open(cat_file_path, 'wb')
+
+    for i, t in enumerate(test_dirs):
+        vid_path = os.path.join(t,"optimal_and_original.mp4")
+        if os.path.exists(vid_path):
+            # print('Processing', i+1, 'of', len(test_dirs), '-', (i+1)/len(test_dirs)*100, "% complete.")
+            line_text = "file '" + vid_path + "'"
+            cat_file.write(vid_path)
+
+
+        else:
+            print("Couldn't process video from", t, "-- because it doesn't exist.")
+            errored_dirs.append(t)
+
+    cat_file.close()
+
+
+    final_vid_path = os.path.join(destination_dir, video_name)
+
+    args = "ffmpeg -f concat -i "+cat_file+" -c copy "+final_vid_path
+    args = shlex.split(args)
+    proc = subprocess.Popen(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    proc.wait()
+
+    return errored_dirs
+
+def copyAllTestVideos(root_tests_dir, destination_dir, videos_to_copy=["optimal_and_original"]):
+    test_name = 'OneComWaypointStaticTest'
+    dirs = [d for d in os.listdir(root_tests_dir) if os.path.isdir(os.path.join(root_tests_dir, d))]
+    test_dirs = sorted([os.path.join(root_tests_dir, d) for d in dirs if re.match(test_name+'.*', d)])
+
+    for i, (t, v) in enumerate(zip(test_dirs,videos_to_copy)):
+        vid_path = os.path.join(t,v+".mp4")
+        if os.path.exists(vid_path):
+            print('Copying', i+1, 'of', len(test_dirs), '-', (i+1)/len(test_dirs)*100, "% complete.")
+            args = "cp " + vid_path + " " + os.path.join(dropbox_dir, str(i).zfill(3)+".mp4")
+            args = shlex.split(args)
+            proc = subprocess.Popen(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            proc.wait()
 
 
 root_tests_dir = os.path.expanduser("~") + "/Optimization_Tests/rand_right_hand_target_tests/bo/"
 dropbox_dir = os.path.expanduser("~") + "/Dropbox/RandReachVideos/"
-test_name = 'OneComWaypointStaticTest'
-dirs = [d for d in os.listdir(root_tests_dir) if os.path.isdir(os.path.join(root_tests_dir, d))]
-test_dirs = sorted([os.path.join(root_tests_dir, d) for d in dirs if re.match(test_name+'.*', d)])
 
-for i, t in enumerate(test_dirs):
-    print('Copying', i+1, 'of', len(test_dirs), '-', (i+1)/len(test_dirs)*100, "% complete.")
-    args = """cp """+t+"""/optimal_and_original.mp4 """+dropbox_dir+"""/"""+str(i).zfill(3)+""".mp4"""
-    args = shlex.split(args)
-    proc = subprocess.Popen(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    proc.wait()
-    # print('Recording simulation for test', i+1, 'of', len(test_dirs), '.', (i+1)/len(test_dirs)*100, "% complete.")
-    # try:
-    #     combineVideos(t)
-    # except:
-    #     print("Couldn't format videos from:\n", t)
+combineVideos(root_tests_dir)
+concatenateTestVideos(root_tests_dir, dropbox_dir, "RandReachVideos.mp4"):
+
+
+
+# test_name = 'OneComWaypointStaticTest'
+# dirs = [d for d in os.listdir(root_tests_dir) if os.path.isdir(os.path.join(root_tests_dir, d))]
+# test_dirs = sorted([os.path.join(root_tests_dir, d) for d in dirs if re.match(test_name+'.*', d)])
+#
+# for i, t in enumerate(test_dirs):
+#     print('Copying', i+1, 'of', len(test_dirs), '-', (i+1)/len(test_dirs)*100, "% complete.")
+#     args = """cp """+t+"""/optimal_and_original.mp4 """+dropbox_dir+"""/"""+str(i).zfill(3)+""".mp4"""
+#     args = shlex.split(args)
+#     proc = subprocess.Popen(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+#     proc.wait()
+#     # print('Recording simulation for test', i+1, 'of', len(test_dirs), '.', (i+1)/len(test_dirs)*100, "% complete.")
+#     # try:
+#     #     combineVideos(t)
+#     # except:
+#     #     print("Couldn't format videos from:\n", t)
