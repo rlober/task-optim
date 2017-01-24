3cb6b2f789f8912a43f4926434836dcf4d8770e2
Ryan Lober <rlober@gmail.com>
2017-01-24 14:35:39 +0100

    test cma

--- optimization-scripts/reaching.py
+++ optimization-scripts/reaching.py
@@ -3,7 +3,8 @@ import os
 from task_optim.test_scenario.one_com_waypoint_static_test import OneComWaypointStaticTest
 from task_optim.solvers.robo_solver import RoboSolver
 from task_optim.solvers.cma_solver import CmaSolver
-from task_optim.video import *
+from task_optim.utils.video import *
+from task_optim.utils.files import *
 
 home_path = os.path.expanduser("~")
 root_path = home_path + "/Optimization_Tests/reaching/"
@@ -20,30 +21,30 @@ costs_to_use = ['tracking', 'goal', 'energy']
 # costs_to_use = ['energy']
 
 maxIter = 50
-
+tolerance = 1e-11
 ####################################################
 
 bo_tests_path = root_path + "bo/"
-first_test = OneComWaypointStaticTest(bo_tests_path, right_hand_starting_waypoints, com_starting_waypoints, costs_to_use)
-
-bo_solver_parameters  = {'max_iter':maxIter, 'tolfun':tolerance, 'par':1000.0, 'kernel':'Matern52', 'acquisition':'EI', 'maximizer':'Direct'}
-solver = RoboSolver(first_test, bo_solver_parameters)
-
-solver.optimize()
-solver.returnSolution(show_simulation=False)
+# first_test = OneComWaypointStaticTest(bo_tests_path, right_hand_starting_waypoints, com_starting_waypoints, costs_to_use)
+#
+# bo_solver_parameters  = {'max_iter':maxIter, 'tolfun':tolerance, 'par':1000.0, 'kernel':'Matern52', 'acquisition':'EI', 'maximizer':'Direct'}
+# solver = RoboSolver(first_test, bo_solver_parameters)
+#
+# solver.optimize()
+# solver.returnSolution(show_simulation=False)
 
 ####################################################
 
-cma_tests_path = root_path + "bo/"
+cma_tests_path = root_path + "cma/"
 first_test = OneComWaypointStaticTest(cma_tests_path, right_hand_starting_waypoints, com_starting_waypoints, costs_to_use)
 
-cma_solver_parameters = {'max_iter':maxIter, 'tolfun':tolerance, 'initial_sigma':0.1}
-solver = CmaSolver(first_test, solver_parameters)
+cma_solver_parameters = {'max_iter':200, 'tolfun':tolerance, 'initial_sigma':0.1}
+solver = CmaSolver(first_test, cma_solver_parameters)
 
 solver.optimize()
 solver.returnSolution(show_simulation=False)
 
 ####################################################
 
-replayOriginalAndOptimalReachingSimulation(bo_tests_path)
-replayOriginalAndOptimalReachingSimulation(cma_tests_path)
+# replayOriginalAndOptimalReachingSimulation(getSortedTestDirs(bo_tests_path)[-1])
+replayOriginalAndOptimalReachingSimulation(getSortedTestDirs(cma_tests_path)[-1])
