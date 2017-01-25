c67527ea77bb7264223a4ab5c4ca940f1bcc2032
Ryan <rlober@gmail.com>
2017-01-25 23:40:57 +0100

    add custom bo solver

--- optimization-scripts/task_optim/solvers/bayes_opt_solver.py
+++ optimization-scripts/task_optim/solvers/bayes_opt_solver.py
@@ -1,100 +1,83 @@
-# import numpy as np
-# import GPy
-# import matplotlib.pyplot as plt
-# from scipy import optimize
-#
-#
-# class BayesOpt:
-#     """docstring for BayesOpt."""
-#     def __init__(self, X_init, Y_init, X_lower, X_upper, cost_saturation=2):
-#         self.cost_saturation = cost_saturation
-#         self.X_init = X_init
-#         self.Y_init = Y_init
-#         self.X_lower = X_lower
-#         self.X_upper = X_upper
-# #        print('x_l:', X_lower, '\n\nx_u:', X_upper)
-#         self.X_range = self.X_upper - self.X_lower
-#         self.input_dimension = self.X_init.shape[1]
-#         self.X = self.normalizeX(self.X_init)
-#         self.Y = self.normalizeY(self.Y_init)
-#
-#         zeros = np.zeros((self.input_dimension,))
-#         ones = np.ones((self.input_dimension,))
-#         self.bounds = list(zip(zeros, ones))
-#         # self.bounds = list(zip(self.X_lower, self.X_upper))
-#
-#         self.gamma = 1.0
-#         self.last_solution =  self.X_init.flatten()
-#         self.solutions = []
-#         self.iteration = 0
-#         # self.shouldOptimize = False
-#         self.shouldOptimize = True
-#
-#         self.gpy_kernel = GPy.kern.RBF(input_dim=self.input_dimension, variance=0.01, lengthscale=0.10)
-#         self.gpy_model = GPy.models.GPRegression(self.X,self.Y,self.gpy_kernel)
-#         self.gpy_model.optimize(messages=False)
-#         self.first_guess = self.minimizeAcquisitionFunction()
-#
-#     def normalizeX(self, X):
-#         x_norm = (X - self.X_lower)/self.X_range
-#  #       print(X, '\n', self.X_lower, '\n', self.X_range, '\n', x_norm, '\n')
-#         return x_norm
-#
-#     def unNormalizeX(self, X):
-#         return X * self.X_range + self.X_lower
-#
-#     def normalizeY(self, Y):
-#         y_norm = Y/self.Y_init
-#         y_norm = np.where((y_norm>self.cost_saturation), self.cost_saturation, y_norm)
-#         return y_norm
-#
-#     def unNormalizeY(self, Y):
-#         return Y*self.Y_init
-#
-#     def getFirstGuess(self):
-#         return self.first_guess
-#
-#     def setBounds(self, bounds):
-#         self.bounds = bounds
-#
-#     def updateModel(self, newX, newY, optimize=False):
-#         self.X = np.vstack((self.X, self.normalizeX(newX) ))
-#         self.Y = np.vstack((self.Y, self.normalizeY(newY) ))
-#
-# #        print("\n\nX:\n", self.X)
-#  #       print("\n\nY:\n", self.Y)
-#         self.gpy_model.set_XY(self.X, self.Y)
-#         if optimize:
-#             self.gpy_model.optimize(messages=False)
-#
-#     def LCB(self, X):
-#         if len(X.shape)==1:
-#             X = np.array([X])
-#         mean, variance = self.gpy_model.predict(X)#, full_cov=False)
-#         return mean - self.gamma * variance
-#
-#     def minimizeAcquisitionFunction(self):
-#
-#   #      print(self.bounds)
-#    #     print(self.normalizeX(self.X_init).flatten())
-#         self.solutions.append(optimize.minimize(self.LCB, x0=self.normalizeX(self.X_init).flatten(), method='L-BFGS-B', bounds=self.bounds))
-#
-#   #      print("normalized solution: ", np.array([self.solutions[-1].x]))
-#         self.last_solution = np.array([self.solutions[-1].x])
-#         self.last_solution = self.unNormalizeX(self.last_solution)
-#         self.last_solution = np.where((self.last_solution > self.X_upper), self.X_upper, self.last_solution)
-#         self.last_solution = np.where((self.last_solution < self.X_lower), self.X_lower, self.last_solution)
-#
-#
-#         self.est_cost_mean, self.est_cost_var = self.gpy_model.predict(self.normalizeX(self.last_solution))
-#         return self.last_solution
-#
-#     def update(self, newX, newY):
-#         self.iteration += 1
-#         print("\n\nIteration: ", self.iteration)
-#         # if (self.iteration % 5) == 0:
-#         #     self.shouldOptimize = True
-#         # else:
-#         #     self.shouldOptimize = False
-#         self.updateModel(newX, newY, self.shouldOptimize)
-#         return self.minimizeAcquisitionFunction()
+from .base_solver import BaseSolver
+import numpy as np
+from sklearn import gaussian_process
+
+class BayesOptSolver(BaseSolver):
+    """docstring for BayesOptSolver."""
+    def __init__(self, test, solver_parameters):
+        assert(solver_parameters['max_iter'] != None)
+
+        self.par = 1.0
+        if 'par' in solver_parameters:
+            self.par = solver_parameters['par']
+        super(BayesOptSolver, self).__init__(test, solver_parameters)
+
+    def initializeSolver(self):
+        self.kernel = 0.5 * gaussian_process.kernels.Matern(length_scale=1.0, length_scale_bounds=(1e-1, 10.0), nu=1.5)
+        self.gp = gaussian_process.GaussianProcessRegressor(self.kernel)
+
+    def updateSolver(self):
+
+        # Put X 0-1
+        X_scaled = self.test.transform(self.X)
+        # Get the next solution to test
+        next_solution_to_test = self.chooseNext(X_scaled, self.Y)
+        # The provided solution is scaled bectween 0-1 so we retransform it back to physical coordinates
+        X_new = self.test.retransform(next_solution_to_test)
+        # test X_new
+        Y_new = self.test.objective_function(X_new)
+        # scale the cost wrt the original cost
+        Y_new = ( Y_new / self.test.Y_init )
+
+        self.X = np.vstack((self.X, X_new))
+        self.Y = np.vstack((self.Y, Y_new))
+
+    def chooseNext(self, X, Y):
+        self.gp.fit(X, Y)
+
+        start_point = np.random.rand(1, X.shape[1])
+
+        # self.verbose_level = -9
+        self.verbose_level = 0
+        self.n_func_evals = 2000
+        self.restarts = 1
+        res = cma.fmin(self.LCB,
+                x0=start_point[0],
+                sigma0=0.8,
+                restarts=self.restarts,
+                options={"bounds": [self.test.X_lower, self.test.X_upper],
+                         "verbose": self.verbose_level,
+                         "maxfevals": self.n_func_evals})
+
+        if res[0] is None:
+            print("CMA-ES did not find anything. \nReturn random configuration instead.")
+            return start_point
+
+        return np.array([res[0]])
+
+
+    def LCB(self, X):
+        mean, variance = self.gp.predict(X, return_std=True)
+        variance = np.reshape(variance, (variance.size, 1))
+        print('mean.shape', mean.shape)
+        print('var.shape', var.shape)
+        lcb = (mean - self.par * np.sqrt(variance))[0,0]
+        print('lcb', lcb)
+        return lcb
+
+    def solverFinished(self):
+        if self.X.shape[0] >= 2:
+            # check for tolerance:
+            if 'tolfun' in self.solver_parameters:
+                deltaSol = np.linalg.norm(self.X[-1,:] - self.X[-2,:])
+                if deltaSol <= self.solver_parameters['tolfun']:
+                    print("Solution tolerance,", self.solver_parameters['tolfun'], "reached. Stopping optimization.")
+                    return True
+
+
+        if (self.test.optimization_iteration <= self.solver_parameters['max_iter']):
+            return False
+
+        else:
+            print("Maximum number of iterations exceeded. Stopping optimization.")
+            return True
