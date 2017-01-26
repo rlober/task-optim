69855dc7bd887c896f0ea28df02326b87e7473de
Ryan <rlober@gmail.com>
2017-01-26 15:38:42 +0100

    running

--- optimization-scripts/rand_right_hand_target_tests.py
+++ optimization-scripts/rand_right_hand_target_tests.py
@@ -6,15 +6,16 @@ from task_optim.solvers.cma_solver import CmaSolver
 from task_optim.solvers.bayes_opt_solver import BayesOptSolver
 
 
-root_path = os.path.join(os.path.expanduser("~"), "/Optimization_Tests/reaching_tests_target_set_01/")
+root_path = os.path.expanduser("~") + "/Optimization_Tests/reaching_tests_target_set_01/"
 
 costs_to_use=['tracking', 'goal', 'energy']
 
 com_starting_waypoints = np.array([[0.015, -0.11, 0.51]])
 
-rh_targets_file_path = os.path.join(os.path.expanduser("~"), "/Code/bayesian-task-optimization/right_hand_targets_set_01.txt")
+rh_targets_file_path = "../right_hand_targets_set_01.txt"
 
 rh_targets = np.loadtxt(rh_targets_file_path)
+
 ######################################################################################
 
 bo_solver_parameters  = {'max_iter':50, 'tolfun':0.01, 'par':10.0}
@@ -22,7 +23,6 @@ bo_test_path = root_path + "/bo/"
 
 cma_solver_parameters  = {'max_iter':100, 'tolfun':1e-15, 'par':0.1}
 cma_test_path = root_path + "/cma/"
-
 
 ######################################################################################
 
@@ -32,10 +32,10 @@ print("=================================================\n\n\n")
 
 for i,t in enumerate(rh_targets):
     print("\n\n========================================")
-    print("Test number:", i+1, "of", n_samples, "tests.\nRight hand target:", t)
+    print("Test number:", i+1, "of", rh_targets.shape[0], "tests.\nRight hand target:", t)
     print("========================================\n\n")
 
-    trial_dir_name = "target_"+str(i).zfill(4)
+    trial_dir_name = "target_"+str(i).zfill(4)+"/"
 
     first_test = OneComWaypointStaticTest(bo_test_path, np.array([t]), com_starting_waypoints, costs_to_use, trial_dir=trial_dir_name)
     solver = BayesOptSolver(first_test, bo_solver_parameters)
