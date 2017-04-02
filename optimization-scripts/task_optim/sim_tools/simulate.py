3dfbc067766b0fb9db08aee67120980603f1a697
Ryan Lober <rlober@gmail.com>
2017-04-03 00:07:39 +0200

    Add gazebo only simulation

--- optimization-scripts/task_optim/sim_tools/simulate.py
+++ optimization-scripts/task_optim/sim_tools/simulate.py
@@ -1,7 +1,7 @@
 import subprocess
 import time
 import shlex
-
+import yarp
 
 def killProcesses():
     proc_list = ["yarpserver", "gzserver", "gzclient", "ocra-icub-server", "reach-client", "stand-client"]
@@ -123,3 +123,103 @@ def simulate(controllerArgs, clientArgs, icubWorldPath, savePath=None, verbose=F
         if verbose:
             print('-- Terminating yarpserver')
         yarp.terminate()
+
+class GazeboSimulation():
+    """A persistant yarp and gazebo process manager"""
+    def __init__(self, world_file_path, verbose=False, visual=False):
+        assert(world_file_path is not None)
+        self.world_file_path = world_file_path
+        self.verbose = verbose
+        self.visual = visual
+        self.yarp_net = yarp.Network()
+        self.yarp_net.init()
+        self.rpc_port = yarp.RpcClient()
+        self.rpc_port_name = "/python/GazeboSimulation/wholeBodyDynamicsTree/rpc:o"
+        self.rpc_port.open(self.rpc_port_name)
+
+        self.launch()
+
+    def __del__(self):
+        self.close()
+
+    def launch(self):
+        if self.verbose:
+            print('-- Launching gzserver with icub.world @', self.world_file_path)
+
+        gz_args = ["gzserver", "-slibgazebo_yarp_clock.so", self.world_file_path, "--verbose"]
+        self.gzserver = subprocess.Popen(gz_args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+        if self.visual:
+            if self.verbose:
+                print('-- Launching visuals with gzclient.')
+            self.gzclient = subprocess.Popen(["gzclient"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+        time.sleep(2.0)
+
+        if self.verbose:
+            print('-- Launching wholeBodyDynamicsTree.')
+        self.wbdtree = subprocess.Popen(["wholeBodyDynamicsTree"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+        time.sleep(5.0)
+
+        if self.yarp_net.connect(self.rpc_port_name, "/wholeBodyDynamicsTree/rpc:i"):
+            self.recalibrateWBDTree()
+        else:
+            print("[ERROR] Couldn't re-calibrate the wholeBodyDynamicsTree.")
+
+    def recalibrateWBDTree(self):
+        msg_btl = yarp.Bottle()
+        reply_btl = yarp.Bottle()
+        msg_btl.addString("resetOffset")
+        msg_btl.addString("all")
+        self.rpc_port.write(msg_btl, reply_btl)
+
+    def reset(self):
+        if self.verbose:
+            print("-- Resetting gazebo simulation environment.")
+        subprocess.Popen(["gz", "world", "-r"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        self.cleanUpYarp()
+        time.sleep(1.0)
+        self.recalibrateWBDTree()
+
+
+    def cleanUpYarp(self):
+        if self.verbose:
+            print('-- Cleaning up ports')
+        args1 = "yarp clean --timeout 0.1"
+        args = shlex.split(args1)
+        cleanYarp = subprocess.Popen(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+
+    def close(self):
+        timeout = 5.0
+        self.rpc_port.close()
+        if self.verbose:
+            print('-- Terminating gzserver')
+
+        self.gzserver.terminate()
+
+        if self.verbose:
+            print('-- Terminating wholeBodyDynamicsTree')
+
+        self.wbdtree.terminate()
+
+        if self.visual:
+            if self.verbose:
+                print('-- Terminating gzclient')
+            self.gzclient.terminate()
+
+        try:
+            self.gzserver.wait(timeout)
+        except:
+            self.gzserver.kill()
+        try:
+            self.wbdtree.wait(timeout)
+        except:
+            self.wbdtree.kill()
+
+        if self.visual:
+            try:
+                self.gzclient.wait(timeout)
+            except:
+                self.gzclient.kill()
